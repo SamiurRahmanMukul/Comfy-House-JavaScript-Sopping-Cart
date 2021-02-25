3c9e974f00efa9ed032d42ec4932086aa8557189
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-02-25 09:06:14 +0600

    Show Cart & Add Item In Cart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 /**
- * =====> TODO: ABOUT AUTHOR <=====
+ * !=====> : ABOUT AUTHOR : <=====!
  *
  * @author : Md. Samiur Rahman (Mukul)
  * @education : B.Sc. Hons In CSE (SIBACS)
- * @profession : PROGRAMMER & SOFTWARE DEVELOPER
+ * @passion : PROGRAMMER & SOFTWARE DEVELOPER
  * @email : sr.mukul9090@gmail.com
  *
  *      </> Happy Coding ☺ </>
@@ -23,6 +23,9 @@ const productsDOM = document.querySelector(".products-center");
 
 // let's cart
 let cart = [];
+
+// buttons
+let buttonsDOM = [];
 
 // TODO: getting the products
 class Products {
@@ -80,12 +83,105 @@ class UI {
       console.log("Error = " + e);
     }
   }
+  getBagButtons() {
+    // bag buttons
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    // console.log(buttons);
+
+    buttonsDOM = buttons;
+
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } else {
+        button.addEventListener("click", (event) => {
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+
+          // *get product from products
+          let cartItem = { ...Storage.getProduct(id), amount: 1 };
+          // console.log(cartItem);
+
+          // *add product to the cart
+          cart = [...cart, cartItem];
+          // console.log(cart);
+
+          // *save cart in local storage
+          Storage.saveCart(cart);
+
+          // *set cart values
+          this.setCartValues(cart);
+
+          // *display cart item
+          this.addCartItem(cartItem);
+
+          // *show the cart
+          this.showCart();
+        });
+      }
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+    // console.log(cartTotal, cartItems);
+  }
+
+  // add item to cart
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+
+    div.innerHTML = `<img src=${item.image} alt="product" />
+
+            <div>
+              <h4>${item.title}</h4>
+              <h5>Price : $ ${item.price}</h5>
+              <span class="remove-item" data-id=${item.id}>remove</span>
+            </div>
+
+            <div>
+              <i class="fa fa-chevron-up" aria-hidden="true" data-id=${item.id}></i>
+              <p class="item-amount">${item.amount}</p>
+              <i class="fa fa-chevron-down" aria-hidden="true" data-id=${item.id}></i>
+            </div>`;
+
+    cartContent.appendChild(div);
+    // console.log(cartContent);
+  }
+  // ? showCart() method create & display cart
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+  }
 }
 
 // TODO: local storage
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  // getProduct() method
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+
+  // save cart item local storage
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
@@ -95,8 +191,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then((products) => {
-    ui.displayProducts(products);
-    Storage.saveProducts(products);
-  });
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
