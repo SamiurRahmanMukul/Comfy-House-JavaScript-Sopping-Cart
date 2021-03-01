d219ab54835c165dde9a82b17391ac1ff5815385
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-03-01 19:22:40 +0600

    Cart Item Increase & Decrease Functionality Makes

--- assets/js/app.js
+++ assets/js/app.js
@@ -191,7 +191,40 @@ class UI {
     clearCartBtn.addEventListener("click", () => {
       this.clearCart();
     });
+
     // cart functionality
+    cartContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild;
+        removeItem.parentElement.parentElement;
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild;
+          lowerAmount.parentElement.parentElement;
+          this.removeItem(id);
+        }
+      }
+    });
   }
   clearCart() {
     let cartItems = cart.map((item) => item.id);
